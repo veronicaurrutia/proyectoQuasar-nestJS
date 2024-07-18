7b2b9ee21f0c886e1b451f2609411b73fbfeb61a
Kurt Muller Cheuquepan <kmuller@loginsa.com>
2024-07-18 18:10:51 -0400

    avance maqueta main layout - menu

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,41 +2,81 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
+        <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
 
         <q-toolbar-title>
-          Quasar App
+          Central de requerimientos
         </q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
+        <q-select class="q-mr-md" dark dense outlined v-model="empresa" :options="['Loginsa', 'otra Empresa']"
+          label="Empresa" />
+
+        <q-select dark dense outlined v-model="area" :options="['tecnologias de la informacion', 'otra area']"
+          label="Area" />
+        <div v-if="false">Quasar v{{ $q.version }}</div>
+
+        <q-item clickable v-ripple>
+          <q-avatar letter color="primary" text-color="white" icon="face" />
+        </q-item>
+
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
+    <q-drawer v-model="leftDrawerOpen" show-if-above :mini="miniState" @mouseover="miniState = false"
+      @mouseout="miniState = true" bordered>
+      <q-scroll-area class="fit" :horizontal-thumb-style="{ opacity: 0 }">
 
-        <EssentialLink
-          v-for="link in linksList"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
+        <q-list darkbordered padding class="text-primary">
+          <q-item v-ripple>
+            <q-item-section avatar>
+              <q-avatar letter color="" text-color="" icon="face" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label lines="1">Kurt Muller </q-item-label>
+              <q-item-label lines="1">kmuller@loginsa.com </q-item-label>
+              <q-item-label lines="1">Administrador </q-item-label>
+
+            </q-item-section>
+
+          </q-item>
+          <q-item v-ripple>
+            <q-item-section>
+              <q-item-label lines="2">Tecnologias de la información. </q-item-label>
+              <q-item-label lines="2">Loginsa. </q-item-label>
+            </q-item-section>
+          </q-item>
+          <q-separator spaced />
+
+          <q-item :active="url === menu.url" @click="url = menu.url" v-for="menu in menu" :key="menu" clickable v-ripple
+            active-class="my-menu-url">
+            <q-item-section avatar>
+              <q-icon :name="menu.icon" />
+            </q-item-section>
+
+            <q-item-section>{{ menu.menu }}</q-item-section>
+          </q-item>
+
+
+
+          <q-separator spaced />
+
+          <q-item clickable v-ripple active-class="my-menu-url">
+            <q-item-section avatar>
+              <q-icon name="settings" />
+            </q-item-section>
+
+            <q-item-section>Configuración</q-item-section>
+          </q-item>
+
+          <q-item clickable v-ripple active-class="my-menu-url">
+            <q-item-section avatar>
+              <q-icon name="logout" />
+            </q-item-section>
+
+            <q-item-section>Salir</q-item-section>
+          </q-item>
+        </q-list>
+      </q-scroll-area>
     </q-drawer>
 
     <q-page-container>
@@ -47,60 +87,60 @@
 
 <script setup>
 import { ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
 
 defineOptions({
   name: 'MainLayout'
 })
 
-const linksList = [
+const area = ref('tecnologias de la informacion')
+const empresa = ref('Loginsa.')
+const url = ref('')
+const menu = ref([
   {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
+    'menu': 'Administración',
+    'icon': 'admin_panel_settings',
+    'url': 'Administración'
   },
   {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
+    'menu': 'dashboard',
+    'icon': 'dashboard',
+    'url': 'dashboard'
   },
   {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
+    'menu': 'nueva solicitud',
+    'icon': 'add_circle',
+    'url': 'nueva solicitud'
   },
   {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
+    'menu': 'requerimientos en proceso',
+    'icon': 'fast_forward',
+    'url': 'requerimientos en proceso'
   },
   {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
+    'menu': 'requerimientos finalizados',
+    'icon': 'check',
+    'url': 'requerimientos finalizados'
   },
   {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
+    'menu': 'requerimientos asignados',
+    'icon': 'supervisor_account',
+    'url': 'requerimientos asignados'
   },
   {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-]
+    'menu': 'gestionar requerimientos',
+    'icon': 'engineering',
+    'url': 'gestionar requerimientos'
+  },
+])
+
+
+
+//let drawer = false
+let miniState = ref(true)
 
 const leftDrawerOpen = ref(false)
 
-function toggleLeftDrawer () {
+function toggleLeftDrawer() {
   leftDrawerOpen.value = !leftDrawerOpen.value
 }
 </script>
